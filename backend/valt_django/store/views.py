0e053170bdd8ee731c9e0e3fa9b1c2fa56ff75b1
Gustavo Sampaio <gustavoabsampaio@gmail.com>
2023-10-15 21:14:38 -0300

    endpoints em progresso

--- backend/valt_django/store/views.py
+++ backend/valt_django/store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from datetime import date
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import  Response
 from .models import Loja, Estilo, Peca, Promocao, Material, Favorita, Procura, EstiloPeca, Segue
 from .serializers import (
     LojaSerializer, EstiloSerializer, PecaSerializer, PromocaoSerializer, 
@@ -9,6 +13,12 @@ from .serializers import (
 class LojaViewSet(viewsets.ModelViewSet):
     queryset = Loja.objects.all()
     serializer_class = LojaSerializer
+    
+    @action(detail=True, methods=['GET'])
+    def get_loja_by_id(sef, request, pk=None):
+        loja = self.get_object()
+        serializer = LojaSerializer(loja)
+        return Response(serializer.data)
 
 class EstiloViewSet(viewsets.ModelViewSet):
     queryset = Estilo.objects.all()
@@ -17,6 +27,13 @@ class EstiloViewSet(viewsets.ModelViewSet):
 class PecaViewSet(viewsets.ModelViewSet):
     queryset = Peca.objects.all()
     serializer_class = PecaSerializer
+    
+    @action(detail=True, methods=['GET'])
+    def get_peca_by_id(sef, request, pk=None):
+        peca = self.get_object()
+        serializer = PecaSerializer(peca)
+        return Response(serializer.data)
+
 
 class PromocaoViewSet(viewsets.ModelViewSet):
     queryset = Promocao.objects.all()
@@ -29,6 +46,22 @@ class MaterialViewSet(viewsets.ModelViewSet):
 class FavoritaViewSet(viewsets.ModelViewSet):
     queryset = Favorita.objects.all()
     serializer_class = FavoritaSerializer
+    
+    @action(detail=True, methods=['POST'])
+    def add_to_favorites(self, request, id_peca, pk=None):
+        try:
+            peca = Peca.objects.get(pk=id_peca)
+            favorita, created = Favorita.objects.get_or_create(
+                id_usuario = request.usuario,
+                peca = peca,
+                data_adicao = date.today(),
+                preco_adicao = request.data.get('preco_adicao')
+                )
+            
+            if created:
+                return Response({'message': 'Produto adicionado aos favoritos.'}, status=201)
+        except Peca.DoesNotExist:
+            return Response({'message': 'Peça não encontrada.'}, status=404)
 
 class ProcuraViewSet(viewsets.ModelViewSet):
     queryset = Procura.objects.all()
--- backend/valt_django/users/views.py
+++ backend/valt_django/users/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.response import  Response
+from rest_framework.decorators import action
 from .models import Usuario
 from .serializers import UsuarioSerializer
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-
-
+    
+    @action(detail=True, methods=['get'])
+    def get_usuario_by_id(sef, request, pk=None):
+        usuario = self.get_object()
+        serializer = UsuarioSerializer(usuario)
+        return Response(serializer.data)

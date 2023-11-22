e2506b37f686d7409dd878bb6331a71466e6b2c5
kingo-blip <kingoblips@gmail.com>
2023-11-22 04:46:22 -0300

    17:00

--- backend/valt_django/users/urls.py
+++ backend/valt_django/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UsuarioViewSet
-
+from .views 
+from django.urls import re_path 
+from . import views
 router = DefaultRouter()
 
 router.register(r'usuarios', UsuarioViewSet)
@@ -12,4 +13,8 @@ router.register(r'usuarios', UsuarioViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token)
+
 ]
